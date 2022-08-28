03aff487ceaf980d7a2967a225259db5727cf5a1
Eduardo Hernandez <edua.24h@gmail.com>
2022-08-28 18:35:14 -0400

    area de contacto html terminado

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,9 @@
+import buttonSend from "./button_send.js";
 import menuHmburgesa from "./menu_hamburgesa.js";
 
 function inite() {
   menuHmburgesa();
+  buttonSend();
 }
 
 window.addEventListener("DOMContentLoaded", inite());
